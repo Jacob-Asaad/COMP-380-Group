5861b24eac83dda8a56bbc83d7e1290930a801b8
Rodolfo Rivera <rorivera0621@gmail.com>
2022-03-29 20:52:02 -0700

    Added icons to bottom tab navigator

--- navigator/ExploreTabs.js
+++ navigator/ExploreTabs.js
@@ -1,19 +1,42 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { View, Image, Text } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import More from '../shared/More';
 import Explore from '../shared/Explore';
 import Profile from '../shared/Profile';
+import CreateWorkout from '../screens/CreateWorkout';
+import {
+    Colors, 
+} from '../components/styles';
 
 const Tab = createBottomTabNavigator();
-
+const { primary, secondary } = Colors;
 const ExploreTabs = () => {
     return(
-        <Tab.Navigator  >
-            <Tab.Screen name = 'Explore' component={Explore}></Tab.Screen>
-            
-            <Tab.Screen name = 'Profile' component={Profile}></Tab.Screen>
-            <Tab.Screen name = 'More' component={More}></Tab.Screen>
+        <Tab.Navigator 
+ 
+        >
+            <Tab.Screen name = 'Explore' component={Explore} options={{
+                tabBarIcon: ({focused}) => (
+                    <MaterialCommunityIcons name = "compass" size={24} color={focused ? primary : secondary }/>
+                ),
+            }}/>
+            <Tab.Screen name = 'Create Workout' component={CreateWorkout} options={{
+                tabBarIcon: ({focused}) => (
+                    <MaterialCommunityIcons name = "weight-lifter" size={24} color={focused ? primary : secondary}/>
+                ),
+            }}/>
+            <Tab.Screen name = 'Profile' component={Profile} options={{
+                tabBarIcon: ({focused}) => (
+                    <MaterialCommunityIcons name = "account" size={24} color={focused ? primary : secondary}/>
+                ),
+            }}/>
+            <Tab.Screen name = 'More' component={More} options={{
+                tabBarIcon: ({focused}) => (
+                    <MaterialCommunityIcons name = "dots-horizontal" size={24} color={focused ? primary : secondary}/>
+                ),
+            }}/>
         </Tab.Navigator>
     );
 }
